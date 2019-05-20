14c57bea56fa48a1b184a108e284332ab4f2fa55
zzn5021 <1310287302.qq@.com>
2019-05-20 09:28:50 +0800

    ZhouKao全部提交

---
+++ ZhouKao/src/main/java/com/zhangzn/util/StreamUtil.java
@@ -0,0 +1,25 @@
+package com.zhangzn.util;
+
+import java.io.File;
+
+import javax.imageio.stream.ImageInputStream;
+
+public class StreamUtil {
+
+  //批量关闭流
+  public static void closeAll() {
+
+  }
+
+  //传入一个文本文件对象
+  public static String readTextFile(ImageInputStream src) {
+    return null;
+
+  }
+
+  //传入文本文件对象，返回该文件内容
+  public static String readTextFile(File file) {
+    return null;
+
+  }
+}
---
+++ ZhouKao/src/test/java/com/zhangzn/Student.java
@@ -0,0 +1,7 @@
+package com.zhangzn;
+
+public class Student {
+
+  String sql = "select * from t_order where create_time>='1' and create_time<='{2}'";
+
+}
